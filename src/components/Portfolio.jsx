4828f3ec591a8bd1f1724e4217013316def87547
onuchukwuchika <chuzksy@gmail.com>
2020-01-09 08:58:22 +0100

    Add the portfolio list page

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 
 import PortfolioCard from './PortofloCard';
+import portfolio from '../data/portfolio';
 
 import '../styles/Portfolio.scss';
 
 const Portfolio = () => (
   <div className="portfolio">
     <div className="container">
-    <h3>Web Developer Portfolio</h3>
-    <PortfolioCard />
+      <h3 className="heading">Web Developer Portfolio</h3>
+      <div className="portfolio-card">
+        {
+          portfolio.data.map((item) => (
+            <PortfolioCard key={item.id} data={item} />
+          ))
+        }
+      </div>
     </div>
   </div>
 );
--- src/components/PortofloCard.jsx
+++ src/components/PortofloCard.jsx
@@ -2,28 +2,22 @@ import React from 'react';
 
 import '../styles/PortfolioCard.scss';
 
-const PortfolioCard = () => (
-  <div className="portfolio-card">
-    <div className="card-content">
-      <img
-        src="https://res.cloudinary.com/dcgwltbei/image/upload/v1578393397/Screenshot_2020-01-07_at_11.36.18.png"
-        alt="EventCity"
-        className="card-img"
-      />
-      <h5>EventCity</h5>
-      <div className="tech-used">
-        <span className="badge badge-info">React</span>
-        <span className="badge badge-info">Redux</span>
-        <span className="badge badge-info">NodeJS</span>
-        <span className="badge badge-info">ExpressJS</span>
-        <span className="badge badge-info">PostgresSQL</span>
-        <span className="badge badge-info">Sequelize</span>
-        <span className="badge badge-info">Selenium</span>
-      </div>
-      <div className="card-foot">
-        <a href="#more">More Info...</a>
-        <a href="#view">View on GitHub</a>
-      </div>
+const PortfolioCard = ({ data }) => (
+  <div className="card-content">
+    <img
+      src={data.imgUrl}
+      alt="EventCity"
+      className="card-img"
+    />
+    <h5>{data.name}</h5>
+    <div className="tech-used">
+      {
+        data.tech.map((item) => <span className="badge badge-info">{item}</span>)
+      }
+    </div>
+    <div className="card-foot">
+      <a href="#more">More Info...</a>
+      <a href={data.githublink}>View on GitHub</a>
     </div>
   </div>
 );
