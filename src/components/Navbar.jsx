44167e5d8e2fcc8c3736fc50d1870e43203ac857
onuchukwuchika <chuzksy@gmail.com>
2020-01-14 15:07:58 +0100

    refactor the navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,44 +4,21 @@ import React, { Component } from 'react';
 import '../styles/Navbar.scss';
 import { Link } from 'react-router-dom';
 
-class Navbar extends Component {
-  state = {
-    isActive: false
-  }
+const Navbar = () => (
 
-  toggleActive = () => {
-    this.setState(prevState => ({
-      isActive: !prevState.isVisible
-    }));
-  };
-
-  render() {
-    const { isVisible } = this.state;
-    return (
-      <nav className="navbar navbar-expand-md navbar-light bg-light">
-        <Link to="/" className="navbar-brand">Chuzksy</Link>
-        <button type="button" className="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarCollapse">
-          <div className="navbar-nav ml-auto">
-            <Link to="/portfolio" className="nav-item nav-link" onClick={this.toggleActive}>Portfolio</Link>
-            <Link to="/about" className="nav-item nav-link" onClick={this.toggleActive}>About</Link>
-            <Link to="/contact" className="nav-item nav-link" onClick={this.toggleActive}>Contact</Link>
-          </div>
-        </div>
-      </nav>
-      // <div className={`nav-header${!isVisible ? "" : " responsive"}`}>
-      //   <Link className="nav-header-link" to="/">Chuzksy</Link>
-      //   <Link className="nav-header-link" to="/portfolio" onClick={this.toggleNavbar}>Portfolio</Link>
-      //   <Link className="nav-header-link" to="/about" onClick={this.toggleNavbar}>About</Link>
-      //   <Link className="nav-header-link" to="/contact" onClick={this.toggleNavbar}>Contact</Link>
-      //   <Link href="#icons" className="icon" onClick={this.toggleNavbar}>
-      //     <i className="fa fa-bars" />
-      //   </Link>
-      // </div>
-    );
-  }
-}
+  <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+    <Link to="/" className="navbar-brand">Chuzksy</Link>
+    <button type="button" className="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+      <span className="navbar-toggler-icon" />
+    </button>
+    <div className="collapse navbar-collapse" id="navbarCollapse">
+      <div className="navbar-nav ml-auto">
+        <Link to="/portfolio" className="nav-item nav-link">Portfolio</Link>
+        <Link to="/about" className="nav-item nav-link">About</Link>
+        <Link to="/contact" className="nav-item nav-link">Contact</Link>
+      </div>
+    </div>
+  </nav>
+);
 
 export default Navbar;
