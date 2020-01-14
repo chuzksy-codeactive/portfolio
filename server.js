58fe48eb7403dd075fd32f38fd8a4188a6981590
onuchukwuchika <chuzksy@gmail.com>
2020-01-14 12:41:33 +0100

    add procfile

--- server.js
+++ server.js
@@ -4,11 +4,7 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(__dirname, +'/dist'));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './dist/index.html'));
-});
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, './dist/index.html'));
