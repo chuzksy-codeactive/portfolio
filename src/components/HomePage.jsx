b742ee5938dde1c5bd55257622857d7be13d8435
onuchukwuchika <chuzksy@gmail.com>
2020-01-31 07:06:32 +0100

    Adjusted the css to show the footer icons without scrolling in the homepage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -8,29 +8,31 @@ const HomePage = () => (
   <div className="homepagecontent">
     <div className="container">
       <div className="content">
-        <img className="content-img" src="https://res.cloudinary.com/dcgwltbei/image/upload/v1579399709/painted_img.jpg" alt="passport" />
-        <h1>Hi, I&apos;m Chika Onuchukwu</h1>
-        <p>
-          I am a Web Developer and a programmer living in Lagos,
-          Nigeria.
-          <br />
-          I make web applications usually with NodeJS and .NET Core
-        </p>
-        <div className="btns">
-          <button className="btn"><Link className="btn-link" to="/portfolio">Portfolio</Link></button>
-          <button className="btn"><Link className="btn-link" to="/project">Current Project</Link></button>
+        <div className="content-items">
+          <img className="content-img" src="https://res.cloudinary.com/dcgwltbei/image/upload/v1579399709/painted_img.jpg" alt="passport" />
+          <h1>Hi, I&apos;m Chika Onuchukwu</h1>
+          <p>
+            I am a Web Developer and a programmer living in Lagos,
+            Nigeria.
+            <br />
+            I make web applications usually with NodeJS and .NET Core
+          </p>
+          <div className="btns">
+            <button className="btn"><Link className="btn-link" to="/portfolio">Portfolio</Link></button>
+            <button className="btn"><Link className="btn-link" to="/project">Current Project</Link></button>
+          </div>
         </div>
-      </div>
-      <div className="footer-icons">
-        <a href="https://linkedin.com/in/onuchukwu-chika" target="_blank" rel="noopener noreferrer">
-          <i className="fa fa-linkedin" />
-        </a>
-        <a href="https://github.com/chuzksy-codeactive" target="_blank" rel="noopener noreferrer">
-          <i className="fa fa-github" />
-        </a>
-        <a href="https://twitter.com/chuzksy" target="_blank" rel="noopener noreferrer">
-          <i className="fa fa-twitter" />
-        </a>
+        <div className="footer-icons">
+          <a href="https://linkedin.com/in/onuchukwu-chika" target="_blank" rel="noopener noreferrer">
+            <i className="fa fa-linkedin" />
+          </a>
+          <a href="https://github.com/chuzksy-codeactive" target="_blank" rel="noopener noreferrer">
+            <i className="fa fa-github" />
+          </a>
+          <a href="https://twitter.com/chuzksy" target="_blank" rel="noopener noreferrer">
+            <i className="fa fa-twitter" />
+          </a>
+        </div>
       </div>
     </div>
   </div>
