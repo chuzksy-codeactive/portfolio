bd3bce35257f71fd4f17ff5db071b93bd99c5153
chuzksy-codeactive <chuzksy@gmail.com>
2020-04-23 12:24:22 +0100

    add current projects to app

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,28 +1,50 @@
 import React from 'react';
-import project from '../data/project';
+import projects from '../data/project';
 
 import '../styles/Project.scss';
+
+const Content = ({ project }) => (
+  <div className="content">
+    <h5><strong>{project.name}</strong></h5>
+    <p>{project.details}</p>
+    <p>{project.motivation}</p>
+    <ul>
+      {project.features && project.features.map((item, i) => (
+        <li key={i}>{item}</li>
+      ))}
+    </ul>
+    <p>
+      <strong>Github link:</strong>
+      <a target="_blank" href={project.github}>
+        {' '}
+        {project.github}
+      </a>
+    </p>
+    {project.website && (
+      <p>
+        <strong>Website: </strong>
+        <a target="_blank" href={project.website}>{project.website}</a>
+      </p>
+    )}
+    {project.demo && <p><strong>Demo</strong></p>}
+    {
+      project.demo && (
+        <div className="portfolio-play">
+          <video autoPlay muted loop className="portfolio-video">
+            <source src={project.demo} type="video/mp4" />
+          </video>
+        </div>
+      )
+    }
+  </div>
+);
 
 const Project = () => (
   <div className="project">
     <div className="container">
-      <div className="content">
-        <h5>{project.name}</h5>
-        <p>{project.details}</p>
-        <p>{project.motivation}</p>
-        <h5>What to learn</h5>
-        <ul>
-          {project.whatToLearn.map((item, i) => (
-            <li key={i}>{item}</li>
-          ))}
-        </ul>
-        <h5>Resources</h5>
-        <ul>
-          {project.resources.map((item, i) => (
-            <li key={i}>{item}</li>
-          ))}
-        </ul>
-      </div>
+      {
+        projects.map((project, i) => <Content key={i} project={project} />)
+      }
     </div>
   </div>
 );
